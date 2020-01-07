3e565f80e4903e8b0a5e44e927dca1de78035929
soonki <soonki@a483e7bf21c6.ant.amazon.com>
2020-01-07 18:11:55 +0900

    initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
+
+export default SearchBar;
